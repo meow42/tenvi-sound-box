160df5c984adcdf967bb89440cf4215c022f18aa
Meow42 <meow42@qq.com>
2024-11-03 19:05:13 +0800

    存储位置变更

--- README.md
+++ README.md
@@ -5,6 +5,5 @@
 本工具仅用于辅助同人游戏开发和怀旧，音乐版权归原作者所有。
 
 ## 相关链接
-- [Tenvi Sound Box 发布地址](https://tenvi.meow42.cn/sound-box/)
+- [Tenvi Sound Box 发布地址](http://tenvi.p1ay4.fun/app/sound/index.html)
 - [Tenvi BGM 歌单](https://y.qq.com/n/ryqq/playlist/8477597037)
-- [包含BGM点播功能的Demo](https://tenvi.meow42.cn/cocos-demo/index.html)
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const vant = window['vant'];
 const soundBox = {
   data() {
     return {
-      domainURL: 'https://tenvi.meow42.cn/res/snd/effect-mp3/',
+      domainURL: 'http://tenvi.p1ay4.fun/res/snd/effect-mp3/',
       //sidebarActive: 0,
       //sidebarData: ['char', 'd-obj', 'etc', 'item', 'map', 'npc', 'skill', 'ui'],
       activeId: 1,
